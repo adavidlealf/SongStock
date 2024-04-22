1aaba318f5e4e465cc2c16294715df6379d0d55a
David Leal <davinchi.leal123@gmail.com>
2024-04-22 03:29:55 -0500

    Creacion del proyecto maven con Springboot Initializr
    Dependencias (de momento):
    Spring Data JPA
    Spring Web
    MySQL Driver
    Lombok

---
+++ songstock-backend/src/test/java/com/poli/songstock/SongstockBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poli.songstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongstockBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
