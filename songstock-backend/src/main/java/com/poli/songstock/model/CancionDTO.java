587958317559ad82a90c610638376a77002f7c40
David Leal <davinchi.leal123@gmail.com>
2024-05-15 09:05:18 -0500

    Test Actualizar cancion

--- songstock-backend/src/main/java/com/poli/songstock/model/CancionDTO.java
+++ songstock-backend/src/main/java/com/poli/songstock/model/CancionDTO.java
@@ -2,4 +2,20 @@ package com.poli.songstock.model;
 
 public class CancionDTO extends ProductoReproducibleDTO{
 
+	private String codigo;
+
+	/**
+	 * @return the codigo
+	 */
+	public String getCodigo() {
+		return codigo;
+	}
+
+	/**
+	 * @param codigo the codigo to set
+	 */
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
 }
