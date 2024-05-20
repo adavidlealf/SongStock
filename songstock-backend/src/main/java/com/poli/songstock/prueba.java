8dad422c8afc3acc503bb52ffecb5882110bfd01
David Leal <davinchi.leal123@gmail.com>
2024-05-20 00:14:37 -0500

    Confirmacion de conexion a motor postgres

--- songstock-backend/src/main/java/com/poli/songstock/prueba.java
+++ songstock-backend/src/main/java/com/poli/songstock/prueba.java
@@ -8,11 +8,11 @@ import org.springframework.context.ApplicationContext;
 public class prueba{
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = SpringApplication.run(AplicacionPrincipal.class, args);
+        /*ApplicationContext applicationContext = SpringApplication.run(AplicacionPrincipal.class, args);
         DataSource dataSource = applicationContext.getBean(DataSource.class);
         
         ConexionPrueba prueba = new ConexionPrueba(dataSource);
-        prueba.probarConexion();
+        prueba.probarConexion();*/
     }
 }
 
