9ce67e2953b61feddb622b80f73587fd27f27929
David Leal <adavidlealf@gmail.com>
2024-05-28 21:09:59 -0500

    Update Users.java

--- songstock-backend/src/main/java/com/poli/songstock/domain/Users.java
+++ songstock-backend/src/main/java/com/poli/songstock/domain/Users.java
@@ -17,8 +17,6 @@ public class Users {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
-	private LocalDate applicantDate;
-	
 	@Column(unique = true)
 	private String nickname;
 	
