69f6b1b2f2270ad58fef4f2b9124e6c2a72fe64c
David Leal <adavidlealf@gmail.com>
2024-05-28 19:10:31 -0500

    Implementacion de createRole y createDefaultRoles en CredentialBusiness
    Se agrego la anotacion @Component en CredentialsBusiness para que sea administrado por SpringBoot.
    
    Metodo createRole privado para ser usado por createDefaultRoles.
    createRole usa RoleService para guardar la entidad Role.
    createDefaultRoles usa RoleService para eliminar todas las entidades Role en caso de que no existan solo 3 entidades Role, y usa createRole para crear cada role.
    
    En el main de SongstockBackendApplication se obtiene el bean de CredentialBusiness para crear los roles despues de ejecutar la aplicacion.

--- songstock-backend/src/main/java/com/poli/songstock/SongstockBackendApplication.java
+++ songstock-backend/src/main/java/com/poli/songstock/SongstockBackendApplication.java
@@ -1,7 +1,12 @@
 package com.poli.songstock;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.poli.songstock.business.CredentialsBusiness;
 
 @SpringBootApplication
 public class SongstockBackendApplication {
@@ -10,4 +15,12 @@ public class SongstockBackendApplication {
 		SpringApplication.run(SongstockBackendApplication.class, args);
 	}
 
+	@Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            CredentialsBusiness credentialsBusiness = ctx.getBean(CredentialsBusiness.class);
+            boolean b = credentialsBusiness.createDefaultRoles();
+            System.out.println("----- Creacion de roles: "+ b);
+        };
+    }
 }
--- songstock-backend/src/main/java/com/poli/songstock/business/CredentialsBusiness.java
+++ songstock-backend/src/main/java/com/poli/songstock/business/CredentialsBusiness.java
@@ -1,8 +1,17 @@
 package com.poli.songstock.business;
 
-import com.poli.songstock.dto.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import com.poli.songstock.domain.Role;
+import com.poli.songstock.dto.UserDTO;
+import com.poli.songstock.service.RoleService;
+
+@Component
 public class CredentialsBusiness {
+	
+	@Autowired
+	private RoleService roleService;
 	
 	/*
 	 * REQUIREMENT 1
@@ -25,6 +34,39 @@ public class CredentialsBusiness {
 	public Object registerProvider(UserDTO userDTO) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	/**
+	 * Crea un rol dado su enum nombre, su lista de palabras concatenadas por coma 
+	 * que puede ver, y su lista de palabras concatenadas por coma que puede modificar.
+	 * @param name Role.Name enum de nombre de rol (solo hay 3 opciones)
+	 * @param canSee String lista de palabras concatenadas por coma que puede ver
+	 * @param canModify String lista de palabras concatenadas por coma que puede modificar
+	 * @return Boolean true si la creacion fue exitosa.
+	 */
+	private Boolean createRole(Role.Name name, String canSee, String canModify) {
+		Role role = new Role();
+		role.setName(name);
+		role.setCanSee(canSee);
+		role.setCanModify(canModify);
+		role = roleService.save(role);
+		return (!LogicBusiness.isEmpty(role) && !LogicBusiness.isEmpty(role.getId()));
+	}
+	
+	/**
+	 * Crea los unicos 3 roles que deben existir por defecto, administrador, consumidor, 
+	 * y distribuidor.
+	 * @return Boolean true si todos los roles se crearon o si no se tuvo que crear ninguno, 
+	 * false si hubo algun error. 
+	 */
+	public Boolean createDefaultRoles() {
+		if(roleService.count()!=3) {
+			roleService.deleteAll();
+			Boolean b = createRole(Role.Name.ADMINISTRATOR, "*", "*");
+			b &= createRole(Role.Name.CONSUMER, null, null);
+			b &= createRole(Role.Name.DISTRIBUTOR, null, null);
+		}
+		return roleService.count()==3;
 	}
 
 }
